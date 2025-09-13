daba432d5fe3028d00e40443314cfac799bbb783
Soorajbhawsar <bhawsarsuraj70@gmail.com>
2025-09-13 12:49:34 +0530

    Update BillRepository.java

--- src/main/java/com/anant/vastralok2/BillRepository.java
+++ src/main/java/com/anant/vastralok2/BillRepository.java
@@ -1,18 +1,22 @@
 package com.anant.vastralok2;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
-public interface BillRepository extends JpaRepository<Bill, Long> {
+public interface BillRepository extends JpaRepository<Bill, BillId> {
+
+    @Query("SELECT b FROM Bill b WHERE b.id.date = ?1")
     List<Bill> findByDate(LocalDate date);
 
-    @Query("SELECT SUM(b.amount) FROM Bill b WHERE b.date = :date")
+    @Query("SELECT SUM(b.amount) FROM Bill b WHERE b.id.date = ?1")
     Double getTotalAmountByDate(LocalDate date);
 
+    @Query("SELECT b FROM Bill b ORDER BY b.id.date DESC, b.id.dailySequence DESC")
     List<Bill> findAllByOrderByDateDesc();
 
+    @Query("SELECT MAX(b.id.dailySequence) FROM Bill b WHERE b.id.date = ?1")
+    Optional<Long> findMaxDailySequenceByDate(LocalDate date);
 }
